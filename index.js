516a836c0a625e9e1a4ea5f8a48a3d90821cb05f
akpiinar <senaa.akpinarr@gmail.com>
2021-01-10 20:40:39 +0300

    css ve kod düzenlemeleri

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-let ileri = document.getElementById("ileri");
-let geri = document.getElementById("geri");
-let daire = document.querySelectorAll('.daire');
-let divider = document.querySelectorAll('.divider');
-let aktif=0;
-console.log(daire.length)
-ileri.addEventListener("click",function(){
-    aktif++;
-    if(aktif>daire.length-1){
-        aktif=daire.length-1;
+var next = document.getElementById("next");
+var prev = document.getElementById("prev");
+var circle = document.querySelectorAll('.circle');
+var divider = document.querySelectorAll('.divider');
+
+var active=0;
+
+next.addEventListener("click",function(){
+
+    active++;
+    if(active>circle.length-1){
+        active=circle.length-1;    
     }
-    daire[aktif].className+=" active";
-    let newAktif = aktif-1;
-    console.log(daire[newAktif]);
-    divider[newAktif].className="divideractive";
+    if(active===3){
+        next.className="disable-button"
+        $("#ileri").prop("disable",true);
+    }
+
+    circle[active].className+=" active";
     
-})
+    let currentActive = active-1;
+
+    console.log(circle[currentActive]);
+    divider[currentActive].className="divideractive";
+    prev.className="";
+
+});
 
 
-geri.addEventListener("click",function(){
-    aktif--;
-    if(aktif<=0){
-        aktif=0;
+prev.addEventListener("click",function(){
+
+    active--;
+    if(active<=0){
+        active=0;
+        $("#geri").prop("disable",true);
+        prev.className="disable-button"
     }
-    daire[aktif].className+= " active";
-    let newAktif = aktif;
-    divider[newAktif].className="divider";
-    newAktif++;
-    daire[newAktif].className="daire";
 
+    next.className=""
+    circle[active].className+= " active";
 
+    let currentActive = active;
 
-})
+    divider[currentActive].className="divider";
+    currentActive++;
+    circle[currentActive].className="circle";
+
+});
 
